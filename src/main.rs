26ba1423be9f67153dc4f1338962374c8ece3e4b
= <florent.klee@outlook.fr>
2024-12-10 22:31:06 +0100

    :recycle: Petite refacto

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,56 @@
+mod wallpaper;
+
 use anyhow::Error;
 use log::LevelFilter;
 use regex::Regex;
-use serde::{Deserialize, Serialize};
 use simple_logger::SimpleLogger;
+use wallpaper::{BingResponse, BASE_URL, BING_API, PATTERN};
 
 fn main() -> anyhow::Result<(), Error> {
     SimpleLogger::new().with_level(LevelFilter::Debug).init()?;
 
-    let base_url = "https://www.bing.com";
-    let url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=fr-FR";
-    let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let response = reqwest::blocking::get(BING_API).unwrap().text().unwrap();
     let bing_response: BingResponse = serde_json::from_str(&response)?;
     let var_url = &bing_response.images[0].url.as_str();
 
-    let title =
-        extract_filename(&bing_response.images[0].urlbase.as_str()).expect("No title found.");
+    log::debug!("{}", &bing_response.images[0].urlbase);
+
+    let title = extract_filename(&bing_response.images[0].urlbase.as_str(), PATTERN)
+        .get(0)
+        .cloned()
+        .expect("blabla");
     let startdate = &bing_response.images[0].startdate.as_str();
 
     let file_name = format!("{startdate}-{title}.png");
 
-    let mut file = std::fs::File::create("img/".to_owned()+file_name.as_str() + ".png")?;
+    let mut file = std::fs::File::create("img/".to_owned() + file_name.as_str() + ".png")?;
 
-    reqwest::blocking::get(base_url.to_owned() + var_url)?.copy_to(&mut file)?;
+    reqwest::blocking::get(BASE_URL.to_owned() + var_url)?.copy_to(&mut file)?;
 
     Ok(())
 }
 
-fn extract_filename(input: &str) -> Option<&str> {
+fn extract_filename(input: &str, pattern: &str) -> Vec<String> {
     log::debug!("{input}");
-    let re = Regex::new(r"(?:\.)([0-9A-Za-z]+)(?:_)").expect("Unable to initiate regex");
-    let caps = re
-        .captures(input)
-        .unwrap_or_else(|| panic!("No value found for title in : {input}"));
-
-    //Group 0 is the whole capture, with . and _ included.
-    //Group 1 is the correct capture. There should not be any more capture than that.
-    if caps.len() > 2 {
-        log::warn!(
-            "Error parsing the url, too many potential filename : {:?}",
-            caps
-        )
+    let re = Regex::new(pattern).expect("Unable to initiate regex");
+    let mut matches = Vec::new();
+    for (_, [cap]) in re.captures_iter(input).map(|c| c.extract()) {
+        matches.push(cap.to_owned());
     }
 
-    let filename = caps.get(1).unwrap().as_str();
-    log::debug!("{}", filename);
-    Some(filename)
+    log::debug!("Number of captured groups : {}", matches.len());
+    matches
 }
 
-#[derive(Serialize, Deserialize)]
-struct BingResponse {
-    images: Vec<BingImages>,
-    tooltips: BingTooltips,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[derive(Serialize, Deserialize)]
-struct BingImages {
-    startdate: String,
-    fullstartdate: String,
-    enddate: String,
-    url: String,
-    urlbase: String,
-    copyright: String,
-    copyrightlink: String,
-    title: String,
-    quiz: String,
-    wp: bool,
-    hsh: String,
-    drk: u32,
-    top: u32,
-    bot: u32,
-    hs: Vec<String>,
-}
+    #[test]
+    fn test_extract_filename() {
+        let test_string = "/th?id=OHR.CornwallSnow_FR-FR1834119825";
+        let extracted_caps = extract_filename(&test_string, PATTERN);
 
-#[derive(Serialize, Deserialize)]
-struct BingTooltips {
-    loading: String,
-    previous: String,
-    next: String,
-    walle: String,
-    walls: String,
+        assert_eq!(extracted_caps.get(0).unwrap(), "CornwallSnow")
+    }
 }
